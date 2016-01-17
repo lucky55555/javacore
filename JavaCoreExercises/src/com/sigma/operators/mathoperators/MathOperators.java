3c3c80fcedece256b61b2a11f1c45ac3c9ca372f
lucky55555 <lxmnprsd@gmail.com>
2016-01-17 11:17:11 -0600

    Checking initial version of MathOperators
    Checking initial version of MathOperators

---
+++ JavaCoreExercises/src/com/sigma/operators/mathoperators/MathOperators.java
@@ -0,0 +1,15 @@
+package com.sigma.operators.mathoperators;
+
+public class MathOperators {
+
+	public static void main(String[] args) {
+		addNumbers(3,4);
+	}
+	
+	public static int addNumbers(int no1, int no2) {
+		int sum = no1+no2;
+		System.out.println("sum:" + sum);
+		return sum;
+	}
+
+}

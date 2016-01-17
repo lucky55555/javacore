9bf8ae8fb735158237f8ac9a96d6b5c84153d815
lucky55555 <lxmnprsd@gmail.com>
2016-01-17 14:47:31 -0600

    Incrementexample

---
+++ IncrementOperators/src/Increment.java
@@ -0,0 +1,14 @@
+
+public class Increment {
+
+	public static void main(String[] args) {
+
+	int i=10;
+	if(i>=23)
+	System.out.println("you are welcome");
+	else 
+		System.out.println("you are not allowed");
+	}
+	}
+
+
